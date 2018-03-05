4e19c2f64d871d4ff70a99017b78f0c2bbc115ac
jhyun94 <hyunjonathan@gmail.com>
2018-03-04 23:26:32 -0600

    setup middleware and action creator for searchWeather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,17 @@
+import axios from 'axios';
+
+const GET_WEATHER = 'get_weather';
+const WEATHER_API = '8ffa32e8fde0194fa77a68dded81e683';
+const URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API}`;
+
+
+
 export function searchWeather(city) {
-	console.log(city);
+
+	const request = axios.get(`${URL}&q=${city},us`);
 	return {
-		type: 'test',
-		payload: []
+		type: GET_WEATHER,
+		payload: request
 	}
 }
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import rootReducer from './reducers/index';
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
 import registerServiceWorker from './registerServiceWorker';
 
-let store = createStore( rootReducer, applyMiddleware())
+import rootReducer from './reducers/index';
+import App from './App';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+let storeWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+
+ReactDOM.render(<Provider store={storeWithMiddleware(rootReducer)}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
