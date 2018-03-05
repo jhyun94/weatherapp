215cfc5274b4644e3a0ecdaf793c75ba917eebf4
jhyun94 <hyunjonathan@gmail.com>
2018-03-04 20:39:34 -0600

    able to create an action on search submit

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export function searchWeather(city) {
+	console.log(city);
+	return {
+		type: 'test',
+		payload: []
+	}
+}
--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class SearchBar extends Component {
+import { searchWeather } from '../actions';
+
+class SearchBar extends Component {
 	constructor(props){
 		super(props)
 
+		this.state = {
+			term: ''
+		}
+		this.submitForm = this.submitForm.bind(this);
+		this.onInputChange = this.onInputChange.bind(this);
 	}
+
+	submitForm(event) {
+		event.preventDefault();
+		this.props.searchWeather(this.state.term);
+		this.setState({ term: ''})
+	}
+
+	onInputChange(event) {
+		this.setState({term: event.target.value})
+	}
+
 	render(){
 		return (
-			<div>searchbar</div>
+			<div>
+				<form onSubmit={ this.submitForm }>
+					<input onChange={this.onInputChange}
+						placeholder="Enter a city" value={this.state.term} />
+					<button type="submit">Enter</button>
+				</form>
+			</div>
 		)
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({searchWeather: searchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
