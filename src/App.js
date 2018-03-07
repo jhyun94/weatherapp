ae423ad91affb909b4e1e0bc0f7802839e719929
jhyun94 <hyunjonathan@gmail.com>
2018-03-07 15:14:46 -0600

    weather list boilerplate

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/search-bar';
+import WeatherList from './components/weather-list';
 
 class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
